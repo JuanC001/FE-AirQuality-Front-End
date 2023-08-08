81b81c313b91ba33e74faf43f47d6c4daf5bd6b9
Juan Camilo Florez <57727114+JuanC001@users.noreply.github.com>
2023-08-08 15:21:40 +0000

    Update AboutAir.jsx

--- src/AboutAir/pages/AboutAir.jsx
+++ src/AboutAir/pages/AboutAir.jsx
@@ -1,13 +1,13 @@
-import { Box, CssBaseline } from '@mui/material'
-import React from 'react'
+import { Box, CssBaseline } from "@mui/material";
+import React from "react";
 
 export const AboutAir = () => {
   return (
     <>
       <CssBaseline />
-      <Box height={'100vh'} bgcolor={'secondary.dark'}>
-
+      <Box height={"100vh"} bgcolor={"secondary.dark"}>
+        a
       </Box>
     </>
-  )
-}
+  );
+};
