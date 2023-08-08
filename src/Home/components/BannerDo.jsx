a9ec346d5c23469c797f4f755602624fe59d650d
JuanC001 <jcflorezp@unbosque.edu.co>
2023-08-07 19:12:54 -0500

    Cambios en Home
    + Nuevas animaciones
    + Cambio de Color
    + Nuevo AppBar
    + Cambion en responsive
    + Nuevas Imagenes

--- src/Home/components/BannerDo.jsx
+++ src/Home/components/BannerDo.jsx
@@ -9,7 +9,7 @@ export const BannerDo = () => {
       <Box width={'99%'} mx={'auto'} mb={5}>
 
         <Stack divider={<Divider variant='middle' />} display={'flex'} alignItems={'center'} my={2}>
-          <Typography variant="h2" color="initial" textAlign={'center'} my={2}><strong>Lo que hacemos</strong></Typography>
+          <Typography variant="h2" color="primary.dark" textAlign={'center'} my={2}><strong>Lo que hacemos</strong></Typography>
           <Typography variant="body1" color="initial" textAlign={'center'} my={2} width={'80%'}> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae minima praesentium quia vel eos architecto id excepturi, quod aliquid dolorem. </Typography>
         </Stack>
 
@@ -17,10 +17,10 @@ export const BannerDo = () => {
 
         <Grid container spacing={4} mt={2}>
 
-          <Grid item xs={6} md={4} height={'30vh'} display={'flex'}>
+          <Grid item xs={12} sm={6} md={4} minHeight={'30vh'} display={'flex'}>
             <CardVM component={Paper} elevation={12} p={1}>
 
-              <Typography variant="h6" color="initial" textAlign={'center'}>Salud Ambiental</Typography>
+              <Typography variant="h6" color="primary.main" textAlign={'center'}>Salud Ambiental</Typography>
               <Divider variant={'middle'} />
 
               <Typography variant="body1" color="initial" textAlign={'justify'} p={3}>
@@ -30,10 +30,10 @@ export const BannerDo = () => {
             </CardVM>
           </Grid>
 
-          <Grid item xs={6} md={4} height={'30vh'} display={'flex'}>
+          <Grid item xs={12} sm={6} md={4} minHeight={'30vh'} display={'flex'}>
             <CardVM component={Paper} elevation={12} p={1} >
 
-              <Typography variant="h6" color="initial" textAlign={'center'}>Economia Circular</Typography>
+              <Typography variant="h6" color="primary.main" textAlign={'center'}>Economia Circular</Typography>
               <Divider variant={'middle'} />
 
               <Typography variant="body1" color="initial" textAlign={'justify'} p={3}>
@@ -43,9 +43,9 @@ export const BannerDo = () => {
             </CardVM>
           </Grid>
 
-          <Grid item xs={12} md={4} height={'30vh'} display={'flex'}>
+          <Grid item xs={12} md={4} minHeight={'30vh'} display={'flex'}>
             <CardVM component={Paper} elevation={12} p={1}>
-              <Typography variant="h6" color="initial" textAlign={'center'}>Conciencia Social</Typography>
+              <Typography variant="h6" color="primary.main" textAlign={'center'}>Conciencia Social</Typography>
               <Divider variant={'middle'} />
 
               <Typography variant="body1" color="initial" textAlign={'justify'} p={3}>
--- src/Home/components/FirstBanner.jsx
+++ src/Home/components/FirstBanner.jsx
@@ -1,7 +1,31 @@
 import { Box, Divider, Grid, Stack, styled, Typography } from '@mui/material'
 import React from 'react'
 
+import { motion } from 'framer-motion'
+
 export const FirstBanner = () => {
+
+    const texts = {
+
+        cantidad: {
+            title: 'Cantidad',
+            text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptas?'
+        },
+        calidad: {
+            title: 'Calidad',
+            text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptas?'
+        },
+        recomendaciones: {
+            title: 'Recomendaciones',
+            text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptas?'
+        },
+        alertas: {
+            title: 'Alertas',
+            text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptas?'
+        }
+
+
+    }
 
     const boxSx = {
         width: {
@@ -13,29 +37,40 @@ export const FirstBanner = () => {
 
     return (
         <>
-            <Box bgcolor={'primary.light'} minHeight={'30vh'} display={'grid'} sx={{ placeContent: 'center' }}>
-                <Stack direction={'row'} divider={<Divider variant='middle' orientation='vertical' flexItem sx={{ borderWidth: '2px', borderRadius: '10px' }} />} gap={{ xs: 2, sm: 2, md: 2, lg: 3 }} useFlexGap flexWrap={'wrap'} textAlign={'center'} py={5}>
+            <Box bgcolor={'secondary.light'} minHeight={'30vh'} display={'grid'} sx={{ placeContent: 'center' }}>
+                <Grid container textAlign={'center'} bgcolor={'primary.main'}>
 
+                    <Grid item xs={12} sm={6} lg={3} display={'flex'} justifyContent={'center'} alignItems={'center'}>
+                        <Stack_item title={texts.cantidad.title} text={texts.cantidad.text} delay={0.2} />
+                    </Grid>
 
-                    <Box sx={boxSx}>
-                        <Typography variant="h5" color="secondary.main" mb={2}><strong>Cantidad</strong></Typography>
-                        <Typography variant="body1" color="initial" textAlign={'justify'} p={2}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam autem eos magnam, mollitia modi voluptatibus id ea, eveniet quod consectetur doloribus corporis impedit expedita velit! Perferendis exercitationem excepturi ea ipsum recusandae at obcaecati distinctio error laudantium explicabo. Itaque, quis sed!</Typography>
-                    </Box>
-                    <Box sx={boxSx}>
-                        <Typography variant="h5" color="secondary.main" mb={2}><strong>Calidad</strong></Typography>
-                        <Typography variant="body1" color="initial" textAlign={'justify'} p={2}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam autem eos magnam, mollitia modi voluptatibus id ea, eveniet quod consectetur doloribus corporis impedit expedita velit! Perferendis exercitationem excepturi ea ipsum recusandae at obcaecati distinctio error laudantium explicabo. Itaque, quis sed!</Typography>
-                    </Box>
-                    <Box sx={boxSx}>
-                        <Typography variant="h5" color="secondary.main" mb={2}><strong>Recomendaciones</strong></Typography>
-                        <Typography variant="body1" color="initial" textAlign={'justify'} p={2}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam autem eos magnam, mollitia modi voluptatibus id ea, eveniet quod consectetur doloribus corporis impedit expedita velit! Perferendis exercitationem excepturi ea ipsum recusandae at obcaecati distinctio error laudantium explicabo. Itaque, quis sed!</Typography>
-                    </Box>
-                    <Box sx={boxSx}>
-                        <Typography variant="h5" color="secondary.main" mb={2}><strong>Alertas</strong></Typography>
-                        <Typography variant="body1" color="initial" textAlign={'justify'} p={2}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam autem eos magnam, mollitia modi voluptatibus id ea, eveniet quod consectetur doloribus corporis impedit expedita velit! Perferendis exercitationem excepturi ea ipsum recusandae at obcaecati distinctio error laudantium explicabo. Itaque, quis sed!</Typography>
-                    </Box>
+                    <Grid item xs={12} sm={6} lg={3} display={'flex'} justifyContent={'center'} alignItems={'center'}>
+                        <Stack_item title={texts.calidad.title} text={texts.calidad.text} delay={0.4} />
+                    </Grid>
 
-                </Stack>
+                    <Grid item xs={12} sm={6} lg={3} display={'flex'} justifyContent={'center'} alignItems={'center'}>
+                        <Stack_item title={texts.recomendaciones.title} text={texts.recomendaciones.text} delay={0.6} />
+                    </Grid>
+
+                    <Grid item xs={12} sm={6} lg={3} display={'flex'} justifyContent={'center'} alignItems={'center'}>
+                        <Stack_item title={texts.alertas.title} text={texts.alertas.text} delay={0.8} />
+                    </Grid>
+
+                </Grid>
             </Box>
         </>
     )
+}
+
+const Stack_item = ({ title, text, delay = 0 }) => {
+
+    return (
+        <motion.div whileInView={{ y: [0, -10, -12, 0] }} transition={{ delay: delay }} viewport={{ once: true }}>
+            <Box bgcolor={'secondary.light'}>
+                <Typography variant="h5" color="primary.main" mb={2}><strong>{title}</strong></Typography>
+                <Typography variant="body1" color="initial" textAlign={'justify'} p={2}>{text}</Typography>
+            </Box>
+        </motion.div>
+    )
+
 }
